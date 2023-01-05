d1c0c3ddcaed267de7834a86be6042c4cb70755f
Yassin Tantawy <yassintantawy3@gmail.com>
2023-01-04 20:22:15 -0800

    added array.every

--- Week_3/advanced-array-methods.js
+++ Week_3/advanced-array-methods.js
@@ -53,16 +53,32 @@
 
     // EXAMPLE: return sum of all elements in array
     arr4 = [2, 4, 6, 2, 8];
-    
+
     let newArr4 = arr4.reduce(function(acc, el){
-        return acc + el; 
+        return acc + el;
     });
 
     console.log(newArr4); // => 22
 
 // ARRAY FIND
-    // The find() method returns the first element in the provided array that satisfies the provided testing function. 
+    // The find() method returns the first element in the provided array that satisfies the provided testing function.
     // If no values satisfy the testing function, undefined is returned.
 
     // SYNTAX
     Array.find(function(element, index, array){});
+
+// ARRAY EVERY
+    // tells whether all elements in the array pass the test implemented by the
+    // provided function
+
+    // SYNTAX
+
+    Array.every(callback(el, index, array));
+
+    // EXAMPLE:
+
+    const isBelowThreshold = (currentValue) => currentValue < 40;
+
+    const array1 = [1, 30, 39, 29, 10, 13];
+
+    console.log(array1.every(isBelowThreshold));// => true
