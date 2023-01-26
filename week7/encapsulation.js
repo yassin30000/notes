37663093024b46b96e250a9022fa576e85ac4265
Yassin Tantawy <yassintantawy3@gmail.com>
2023-01-25 20:16:25 -0800

    classes, encapsulation

---
+++ week7/encapsulation.js
@@ -0,0 +1,17 @@
+// ENCAPSULATION
+
+    // definition
+        // the idea of hiding states from users
+        // bringing together data and the methods which operate on that data into a single unit
+
+    // notes
+
+        // common use for encapsulation is keeping states private from users
+        // we could do this by having private objects and public objects
+
+        // public objects
+            // accessable by other objects
+
+        // private objects
+            // only accessible by the object that it is in
+            // closures are private objects, or 'encapsulated data'
